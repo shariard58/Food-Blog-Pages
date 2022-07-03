ad1b2a5b60128d3858846da7d027e3c87547ed3d
shariard58 <shariard58@gmail.com>
2022-07-04 05:08:32 +0600

    Done with thdesign

---
+++ app.js
@@ -0,0 +1,8 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+mobileMenu.addEventListener("click", () => {
+  document.querySelector(".menu").classList.toggle("show");
+});
+
+console.log("shariar");
+
+// js for navigation toggle
